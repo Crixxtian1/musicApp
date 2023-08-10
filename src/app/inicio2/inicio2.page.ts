f0e827f484d4dc9b62c0e25f33e3305a1f57d7dc
aryflavio <arianny.flavio@etec.sp.gov.br>
2023-08-10 12:17:18 -0300

    arrumamos inicio

--- src/app/inicio2/inicio2.page.ts
+++ src/app/inicio2/inicio2.page.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './inicio2.page.html',
   styleUrls: ['./inicio2.page.scss'],
 })
-export class Inicio2Page implements OnInit {
+export class Inicio2Page{
 
-  constructor() { }
+  showInicio2 = false;
+  selectedOption: string | undefined;
 
-  ngOnInit() {
+  toggleInicio2() {
+    this.showInicio2 = !this.showInicio2;
   }
 
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.showInicio2 = false;
+  }
 }
+
+
