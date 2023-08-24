06e0310d7f537a58e12e821b1737d623502ac4ce
aryflavio <arianny.flavio@etec.sp.gov.br>
2023-08-24 09:02:59 -0300

    tabela de cadastro

--- src/app/tabelasdecadastro/tabelasdecadastro.page.ts
+++ src/app/tabelasdecadastro/tabelasdecadastro.page.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabelasdecadastroPage implements OnInit {
 
+  user = {
+    name: 'Arianny',
+    city: 'Lençois',
+    bio: 'Biografia do Usuário',
+    address: 'joquim gomes machado, 555',
+    imageUrl: '../../assets/usuario.png',
+  };
+
   constructor() { }
 
   ngOnInit() {
