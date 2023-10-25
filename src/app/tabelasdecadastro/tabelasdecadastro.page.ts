e6a97c0e5e6e1f208e8e127a5c98bc091156ec53
Crixxtian1 <cristianpgo123321@gmail.com>
2023-10-25 15:23:34 -0300

    comentario

--- src/app/tabelasdecadastro/tabelasdecadastro.page.ts
+++ src/app/tabelasdecadastro/tabelasdecadastro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MusicaapiService } from '../services/musicaapi.service';
 import { ProfessorService } from '../services/professor.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tabelasdecadastro',
@@ -9,11 +10,19 @@ import { ProfessorService } from '../services/professor.service';
 })
 export class TabelasdecadastroPage implements OnInit {
 
+  mostrarComentarios = false;
+  novoComentario: string = '';
+  nomeUsuario = 'Isabelli Kevia da Silva';
+  fotoUsuario = '../../assets/isa.png'; // Substitua pela URL da foto do usuário
+  comentarios: { nome: string, foto: string, texto: string, horario: Date }[] = [];
+
+
   instrumento = ''
   data: any = {}
   dataResultados: any = []
 
-  constructor(private musicaapiService: MusicaapiService, private professorService: ProfessorService) { }
+  constructor(private musicaapiService: MusicaapiService, private professorService: ProfessorService, private storage: Storage) {
+  }
 
   async ngOnInit() {
     this.instrumento = this.professorService.getInstrumento()
@@ -26,6 +35,34 @@ export class TabelasdecadastroPage implements OnInit {
     this.dataResultados = this.data.content.filter((d: any) => d.cidade.toLowerCase().indexOf(query) > -1);
   }
 
+  toggleComentarios() {
+    this.mostrarComentarios = !this.mostrarComentarios;
+    this.carregarComentarios();
+  }
 
+  adicionarComentario() {
+    if (this.novoComentario.trim() !== '') {
+      this.comentarios.unshift({
+        nome: this.nomeUsuario,
+        foto: this.fotoUsuario,
+        texto: this.novoComentario,
+        horario: new Date()
+      });
+      this.novoComentario = '';
+      this.salvarComentarios();
+    }
+  }
 
+  async carregarComentarios() {
+    const comentarios = await this.storage.get('comentarios');
+    if (comentarios) {
+      this.comentarios = comentarios;
+    }
+  }
+
+  salvarComentarios() {
+    this.storage.set('comentarios', this.comentarios);
+  }
 }
+
+
